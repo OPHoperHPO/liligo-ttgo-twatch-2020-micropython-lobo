960806c73fda816291d5ae9362094f6dbc3230b6
Anodev <29470622+ophoperhpo@users.noreply.github.com>
2020-07-17 02:23:21 +1000

    Добавлен порт официальной библиотеки TTGO_TWatch_Library

--- MicroPython_BUILD/components/micropython/esp32/modules/ttgo.py
+++ MicroPython_BUILD/components/micropython/esp32/modules/ttgo.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 # Порт официальной библиотеки ttgo для часов LilyGo TTGO T-Watch 2020.
 # Автор: Nikita Selin (Anodev)[https://github.com/OPHoperHPO]
+import lvgl as lv
+import lvgl_helper as lv_h
+import lvesp32
 import display
 from ft5206 import FT5206
 import bma423 as BMA423
@@ -13,11 +16,13 @@ class TTGO:
     def __init__(self):
         self.__i2c__ = I2C(scl=22, sda=21, speed=400000)
         self.pmu = axp202.PMU(self.__i2c__)
+        self.init_power()
         self.rtc = PCF8563(self.__i2c__)
         self.tft = self.__init_display__()
         self.bma = self.__init_bma__()
         self.__i2c2__ = I2C(id=1, scl=32, sda=23, speed=400000)
         self.touch = FT5206(self.__i2c2__)
+        self.motor = Motor()
 
     def __init_bma__(self):
         BMA423.init(self.__i2c__)
@@ -25,6 +30,31 @@ class TTGO:
 
     def __init_display__(self):
         return Display(self.pmu)
+
+    def rtc_attach_interrupt(self, rtc_callback):
+        Pin(37, mode=Pin.IN, handler=rtc_callback, trigger=Pin.IRQ_FALLING)
+
+    def enable_audio_power(self, en=True):
+        self.pmu.setLDO3Mode(1)
+        self.pmu.setPowerOutPut(axp202.AXP202_LDO3, en)
+
+    def lvgl_begin(self):
+        lv.init()
+        disp_buf1 = lv.disp_buf_t()
+        buf1_1 = bytes(240 * 10)
+        lv.disp_buf_init(disp_buf1, buf1_1, None, len(buf1_1) // 4)
+        disp_drv = lv.disp_drv_t()
+        lv.disp_drv_init(disp_drv)
+        disp_drv.buffer = disp_buf1
+        disp_drv.flush_cb = lv_h.flush
+        disp_drv.hor_res = 240
+        disp_drv.ver_res = 240
+        lv.disp_drv_register(disp_drv)
+        indev_drv = lv.indev_drv_t()
+        lv.indev_drv_init(indev_drv)
+        indev_drv.type = lv.INDEV_TYPE.POINTER
+        indev_drv.read_cb = self.touch.lvgl_touch_read
+        lv.indev_drv_register(indev_drv)
 
     def init_power(self):
         # Change the button boot time to 4 seconds
@@ -89,6 +119,15 @@ class Display:
         if 0 <= percent <= 100:
             voltage = 800 * percent / 100
             self.pmu.set_lcd_backlight_voltage(2400 + voltage)
+
+    def display_off(self):
+        self.tft.tft_writecmd(0x10)
+
+    def display_sleep(self):
+        self.tft.tft_writecmd(0x10)
+
+    def display_wakeup(self):
+        self.tft.tft_writecmd(0x11)
 
     def backlight(self, val):
         self.tft.backlight(val)
